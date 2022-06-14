f8706d995444aac00363cfc6857a7a655a0077df
marksverdhei <marksverdhei@hotmail.com>
2022-06-15 00:04:17 +0200

    Add html collection script

---
+++ collect_archivetoday.py
@@ -0,0 +1,86 @@
+import os
+
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from tqdm import tqdm
+from time import sleep
+
+chrome_options = Options()
+# This line disables javascript
+chrome_options.add_experimental_option("prefs", {'profile.managed_default_content_settings.javascript': 2})
+
+DATA_PATH = "data/html/archive_today"
+captcha_content = "Completing the CAPTCHA proves you are a human"
+
+
+def get_downloaded_document_ids():
+    "Removes extensions from html files: abc123.html -> abc123"
+    return [i[:-5] for i in os.listdir(DATA_PATH)]
+
+
+def is_captcha_blocked(document):
+    return captcha_content in document
+
+
+def download_pages(data):
+    """
+    Downloads pages using selenium web driver
+    """
+
+    n_retries_threshold = 5
+    n_retries = 0
+
+    while (len(data) > 0) and (n_retries < n_retries_threshold):
+        driver = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
+        
+        for i in tqdm(data.index):
+            try:
+                url = data.loc[i]["url"]
+                
+                driver.get(url)
+                content = driver.page_source
+
+                assert not is_captcha_blocked(content)
+                
+                with open(f"{DATA_PATH}/{i}.html", "w+", encoding="UTF8") as f:
+                    f.write(content)
+
+                # We delay each request with
+                # 1 second to not overload the site
+                sleep(1)
+
+            except Exception as e:
+                print("Error occurred:")
+                print(str(e)[:500])
+                with open("error.log", "a") as f:
+                    f.write(str(e))
+
+                print(f"Skipping {i} for now...")
+                driver.quit()
+                driver = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
+
+        local_doc_ids = get_downloaded_document_ids()
+        data.drop(local_doc_ids, inplace=True, errors="ignore")
+
+        n_retries += 1
+        driver.quit()
+
+
+def main():
+    html_dir = "data/html"
+    archive_today_dir = f"{html_dir}/archive_today"
+
+    for path in html_dir, archive_today_dir:
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+
+    data = pd.read_csv("data/intermediate/archivetoday_urls.tsv", index_col=0, sep="\t")
+    local_doc_ids = get_downloaded_document_ids()
+    data.drop(local_doc_ids, inplace=True, errors="ignore")
+    download_pages(data)
+
+
+if __name__ == "__main__":
+    main()
---
+++ collect_webarchive.py
@@ -0,0 +1,78 @@
+import logging
+import os
+import pickle
+from typing import Dict
+
+import pandas as pd
+import requests
+from requests import Response
+from tqdm import tqdm
+
+TOO_MANY_REQUESTS_TIMEOUT = 12
+
+
+def fetch_responses(data: pd.DataFrame) -> Dict[str, Response]:
+    """
+    Uses requests to retrieve responses
+    """
+
+    responses = {}
+    tmr_counter = 0
+
+    try:
+        for id in tqdm(data.index):
+            url = data["url"].loc[id]
+            logging.info(url)
+
+            try:
+                response = requests.get(url)
+                logging.info(response)
+
+                if response.status_code == 429:
+                    tmr_counter += 1
+                else:
+                    tmr_counter = 0
+
+                if tmr_counter >= TOO_MANY_REQUESTS_TIMEOUT:
+                    raise TimeoutError("You're busted! 429 12 times in a row!")
+            except (
+                requests.exceptions.SSLError,
+                requests.exceptions.ConnectionError,
+                requests.exceptions.ChunkedEncodingError,
+            ) as e:
+                logging.warning(e)
+                continue
+
+            responses[id] = response
+    except Exception as e:
+        logging.error(e)
+    finally:
+        return responses
+
+
+def main():
+    html_dir = "data/html"
+    web_archive_dir = f"{html_dir}/web_archive"
+
+    for path in html_dir, web_archive_dir:
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+    data = pd.read_csv("data/intermediate/webarchive_urls.tsv", index_col=0, sep="\t")
+
+    downloaded = [i[:-5] for i in os.listdir(web_archive_dir)]
+    data.drop(downloaded, inplace=True, axis=0, errors="ignore")
+
+    responses = fetch_responses(data)
+
+    for id, response in tqdm(responses.items()):
+        if response.status_code == 200:
+            with open(f"{web_archive_dir}/{id}.html", "wb+") as f:
+                f.write(response.content)
+
+    with open("data/intermediate/requests_responses.pickle", "wb+") as f:
+        pickle.dump(responses, f)
+
+
+if __name__ == "__main__":
+    main()
